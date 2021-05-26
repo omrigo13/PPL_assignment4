3d9d6cd605e657b6211a6dcfba95b07388859c98
Omri Goldberg <goldomri@post.bgu.ac.il>
2021-05-26 12:43:43 +0300

    completed tasks 2.1-2.3

--- part2/src/part2.ts
+++ part2/src/part2.ts
@@ -9,42 +9,59 @@ type PromisedStore<K, V> = {
 }
 
 
-// export function makePromisedStore<K, V>(): PromisedStore<K, V> {
-//     ???
-//     return {
-//         get(key: K) {
-//             ???
-//         },
-//         set(key: K, value: V) {
-//             ???
-//         },
-//         delete(key: K) {
-//             ???
-//         },
-//     }
-// }
+export function makePromisedStore<K, V>(): PromisedStore<K, V> {
+    let store_data = new Map()
+    return {
+        get(key: K) {
+            return store_data.has(key) ? Promise.resolve(store_data.get(key)) : Promise.reject(MISSING_KEY)
+        },
+        set(key: K, value: V) {
+            store_data = store_data.set(key, value)
+            return Promise.resolve()
+        },
+        delete(key: K) {
+            return store_data.delete(key) ? Promise.resolve() : Promise.reject(MISSING_KEY)
+        },
+    }
+}
 
-// export function getAll<K, V>(store: PromisedStore<K, V>, keys: K[]): ??? {
-//     ???
-// }
+export function getAll<K, V>(store: PromisedStore<K, V>, keys: K[]): Promise<V[]> | Promise<void> {
+    return Promise.all(keys.map((key) => store.get(key)))
+}
 
 /* 2.2 */
 
-// ??? (you may want to add helper functions here)
-//
-// export function asycMemo<T, R>(f: (param: T) => R): (param: T) => Promise<R> {
-//     ???
-// }
+export function asycMemo<T, R>(f: (param: T) => R): (param: T) => Promise<R> {
+    let store = makePromisedStore<T, R>()
+    let result:(param: T) => Promise<R> = (param: T) => {
+        return store.get(param).then((val: R) => val, () => {
+            let func_result: R = f(param);
+            store.set(param, func_result)
+            return func_result
+        })
+    }
+    return result
+}
 
 /* 2.3 */
 
-// export function lazyFilter<T>(genFn: () => Generator<T>, filterFn: ???): ??? {
-//     ???
-// }
+export function lazyFilter<T>(genFn: () => Generator<T>, filterFn: (param:T)=>boolean): ()=> Generator<T> {
+    function* generator() {
+        for (let val of genFn()) {
+            if(filterFn(val)) yield val
+        }
+    } 
+    return generator;
+}
 
-// export function lazyMap<T, R>(genFn: () => Generator<T>, mapFn: ???): ??? {
-//     ???
-// }
+export function lazyMap<T, R>(genFn: () => Generator<T>, mapFn: (param:T)=>R): ()=> Generator<R> {
+    function* generator() {
+        for (let val of genFn()){
+            yield mapFn(val);
+        }
+    }
+     return generator;
+}
 
 /* 2.4 */
 // you can use 'any' in this question
